c34bf979d5dbcdfb40ce180e4ffd041160e9ea19
Sophia Wassermann <so.wassermann@gmail.com>
2019-05-10 15:21:50 +0100

    Created a project for my stickleback videos

--- deeplabcut_fishshoal.py
+++ deeplabcut_fishshoal.py
@@ -2,13 +2,13 @@ import deeplabcut
 
 task = "Stickleback Shoaling"  # Enter the name of your experiment Task
 experimenter = "Sophie"  # Enter the name of the experimenter
-video=["videos/video1.avi", "videos/video2.avi"]  # Enter the paths of your videos you want to grab frames from.
+video=["tracking_videos/sticklebacks1.mp4"]  # Enter the paths of your videos you want to grab frames from.
 
-path_config_file=deeplabcut.create_new_project(task,
-                                               experimenter,
-                                               video,
-                                               working_directory="Full path of the working directory",
-                                               copy_videos=True/False)  # change the working directory to where you want the folders created.
+path_config_file = deeplabcut.create_new_project(task,
+                                                 experimenter,
+                                                 video,
+                                                 working_directory="sticklebacks",
+                                                 copy_videos=False)  # change the working directory to where you want the folders created.
 
 # The function returns the path, where your project is.
 # You could also enter this manually (e.g. if the project is already created and you want to pick up, where you stopped...)
